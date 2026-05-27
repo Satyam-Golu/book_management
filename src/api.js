2e8c396f341afe09a928bdf883be746ee541e07b
Satyam-Golu <112718520+Satyam-Golu@users.noreply.github.com>
2026-05-27 23:54:49 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://lumina-backend-zjud.onrender.com';
+const API_URL = 'https://lumina-backend-zjud.onrender.com/books';
 
 export const getBooks = async () => {
   const response = await axios.get(API_URL);
